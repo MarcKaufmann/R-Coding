e1a57c7be5eb1c521ca01dc4d74c49cd7cb5dd19
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2019-10-14 13:23:10 +0200

    Notes for class

--- lecture5/lecture5.R
+++ lecture5/lecture5.R
@@ -1,1 +1,15 @@
 # Script for lecture 5
+
+# Team 4
+# Péter
+# Alexandra
+# Kristof
+# Miklos
+
+# Points:
+# 1. Don't list all functions, e.g. geoms
+# 2. Exam in DA1
+# 3. Look at file before submitting
+# 4. ggsave
+# 5. using vienna code and theming
+# 6. Team: How to assign/report exercises
