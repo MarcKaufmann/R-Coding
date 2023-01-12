e98a3d8111a1404dfead9564332641521bcafc39
Marc <marc@trichotomy.xyz>
2023-01-12 17:38:35 +0100

    notes from class 1

--- lecture1/lecture1-class-script.R
+++ lecture1/lecture1-class-script.R
@@ -1,18 +1,18 @@
-## Intro to R: Class 1
-
-## Coding for Economics/Stata/Scientific Python/machine learning/SPSS
-
-# Things To Know about R
 # 1. Everything has a name
-2
+# Basic Arithmetic
+2 
 4 + 7
 7/3
 7 %% 3
+7//3
+7 %/% 3
 x^2
 2^2
-2^3
-# When you are on a line, hit <ctrl-enter>
-2**3
+2*2
+# caret (^)
+3*2
+3^2
+3**2
 
 # Pre-defined variables
 pi
@@ -20,76 +20,108 @@ pi
 'pi'
 "pi" == 'pi'
 TRUE
-True
 true
 FALSE
-F
-T
-3
-3 == 4
-TRUE
-FALSE == FALSE
 
 # Pre-defined functions
 c
+c(0)
+0
+0 == c(0)
+c(0, 1)
+c(0, 0, 0, 0, 0, 0)
+help(c)
+"c"
+"c(0)"
 C
-"C"
+c
 
 # 2. Everything is an object
 
-marcs_new_object <- "I am a fancy object"
+marcs_new_object <- "I am a fancy object" # a = 1; a == 1; 1 == a; 1 = a
 marcs_new_object
+
 marcs_new_combined_object <- c(marcs_new_object, "Some apples, because they are healthy")
-marcs_new_combined_object                               
-c(1, "and this", "and that")
-c(1, 2, 3)
-range(100)
-seq(100)
-seq(10)
-rep("This and that", 10)
-?seq
-seq(1, 300, 5)
-?seq
-seq()
-seq(from=1, to=300, by=5)
-seq(to=300, from=1, by=5)
-seq(300, 1, 5)
-seq(300, 1, -5)
+marcs_new_combined_object
 
-seq
+list_of_p_languages <- c(
+  "Racket",
+  "SQL",
+  "R",
+  "Python",
+  "Javascript",
+  "Matlab",
+  "Julia",
+  "Maple", # Maths
+  "Processing", # Graphics
+  "assembly",
+  "C", # The mother of all modern languages
+  "C++",
+  "C#",
+  "Scratch", # For Kids (and adults who remain kids)
+  "DAX" # Power BI
+)
+length(list_of_p_languages)
+languages_heard_of <- c(
+  10,
+  12,
+  8,
+  11,
+  11,
+  10,
+  10,
+  7,
+  10,
+  10,
+  9,
+  8,
+  11,
+  9,
+  9,
+  10,
+  10,
+  14
+)
 
-# You do things using functions
+# 3. You do things using functions
 
 mean
-mean()
-?mean
 mean(c(0, 10))
+mean(languages_heard_of)
 mean("a")
-?NA
 NA
-na
-NA
-mean(c(0, 10, NA)) # 5    3.33  NA   0
-mean(c(0, 10, NA), na.rm = TRUE)
-mean(c(0, 10, "super rich"))
+help(NA)
+mean(c(0, NA))
+mean(c(0, NA), na.rm = TRUE)
 
 # 4. Functions come in packages
 
 ggplot
 library(ggplot2)
-ggplot
-tibble
-??tibble
-tibble::tibble
-?library
-?tibble
-??tibble
-library("tidyverse")
+# install.packages("ggplot2")
+ggplot()
 
-library(wishful_thinking)
-install.packages(wishful_thinking)
-install.packages('wishful_thinking')
+# Importing can mask other definitions
+library(dplyr) # masks filter and lag (among others)
 
-x <- c(1, 3.0, 2.9)
+# How to figure out what is what
+x <- c(0, 3.0, 2.9)
 x
 str(x)
+x[[1]]
+x[[0]]
+x[c(1,2)]
+y <- c(0, 1)
+y <- c("1", "3.0", "2.9")
+y
+str(y)
+class(y)
+z <- c("1", 1)
+z
+w <- list("1", 1)
+w
+str(w)
+
+# help
+
+help(summary)
