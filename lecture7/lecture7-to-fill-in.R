302fe6e16a8f4ca9ac4a5114c785d2d2c18b33a3
Marc <marc@trichotomy.xyz>
2024-02-22 17:10:13 +0100

    lecture 7: after class

--- lecture7/lecture7-to-fill-in.R
+++ lecture7/lecture7-to-fill-in.R
@@ -1,11 +1,28 @@
 library(tidyverse)
 
-# 12.3.1 Gathering
+# 12.3.1 pivot-longer
 
 table4a # This is defined 
+?table4a
+# country | year | cases 
+# afg    | 1999 | 745
+# afg    | 2000 | 2666
+# ...
+# as.character(2000:2010)
+(tidy4a <- table4a |>
+  pivot_longer(cols = c("1999", "2000"), names_to = "Year", values_to = "Cases"))
 
+table4b
+(tidy4b <- table4b |>
+  pivot_longer(
+    cols = c("1999", "2000"), 
+    names_to = "Year", 
+    values_to = "Population"
+  ) |>
+  mutate(Year = as.integer(Year)))
 
 # We will see more on joins in the coming weeks
+(tidy4 <- left_join(tidy4a, tidy4b))
 
 
 
@@ -16,12 +33,22 @@ stocks <- tibble(
   return = c(1.88, 0.59, 0.92, 0.17)
 )
 
+stocks |>
+  pivot_wider(names_from = year, values_from = return) |>
+  pivot_longer(
+    cols = c("2015", "2016"), 
+    names_to = "year", 
+    values_to = "return",
+    names_transform = list(year = as.double)
+  )
+  
+stocks
 
 # Exercise 12.3 the second
 # Why does this code fail?
 
 table4a |> 
-  pivot_longer(cols = c(1999, 2000), names_to = "year", values_to = "cases")
+  pivot_longer(cols = c("1999", "2000"), names_to = "year", values_to = "cases")
 
 # Exericse 3 in 12.3
 # Why does widening this table fail? How could adding an additional column help?
@@ -47,8 +74,21 @@ preg <- tribble(
   "no",      20,    12
 )
 
+# 1. What are the columns I want at the end?
+# pregnant, sex, count
+# 2. Where does the data for each column come (cols = ...) from 
+#    and then go to (names_to = ..., values_to = ...)?
+
+preg |>
+  pivot_longer(cols = c("male", "female"), names_to = "sex", values_to = "count")
+
 ## Separate and Unite: One column <-> many columns
 table3
+(table3sep <- table3 |>
+  separate(rate, into = c("cases", "population"), convert = TRUE))
+
+table3sep |>
+  unite(new, cases, population, sep="/")
 
 
 ## Small Test 
@@ -57,14 +97,24 @@ table3
 # People answered 6 different questions, wtatest, wta{1,2,3,4,5}. 
 # Test, with t.tests, whether the mean answers to these questions are different. 
 
-df <- read_csv("some_choices.csv")
+df <- read_csv("lecture7/some_choices.csv")
+df
 
-# Let's spread by question_name
+
+# Let's pivot_wider by question_name
 df2 <- df |>
-  ...
+  pivot_wider(names_from = question_name, values_from = answer)
+
+is_not_na <- function(x) {
+  !is.na(x) 
+}
+
+df2[1,2] <- NA
+df2
 df3 <- df2 |>
   # Keep only those that have no missing values for any answer
-  ...
+  #filter(!is.na(wtatest), !is.na(wta1), ...)
+  filter( if_all(starts_with("wta"), is_not_na) )
 
 # Or cleaner
 df4 <- df2 |>
